c9a548d871021f21026cd6f97ed1849427aedfbc
Parth Pandya <parth.pandya@valensdatalabs.com>
2020-01-07 19:31:25 +0530

    Adds two new problems

--- Problem-3.py
+++ Problem-3.py
@@ -18,3 +18,23 @@ node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
 
 '''
+
+import re
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return "%s<%s,%s>" % (str(self.val), str(self.left), str(self.right))
+
+
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+def serialize(tree):
+    return str(tree)
+
+node_serialized = serialize(node)
+print(node_serialized)
