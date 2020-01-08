a2381c881b0426cccdb57dbce0efbe1dccbfc23a
Parth Pandya <parth.pandya@valensdatalabs.com>
2020-01-08 20:08:57 +0530

    Adds Problem-3 solution

--- Problem-3.py
+++ Problem-3.py
@@ -28,13 +28,46 @@ class Node:
         self.right = right
 
     def __repr__(self):
-        return "%s<%s,%s>" % (str(self.val), str(self.left), str(self.right))
+        return "Node(%s, %s, %s)" % (self.val, self.left, self.right)
 
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 
-def serialize(tree):
-    return str(tree)
+def serialize(tree, index="1"):
+    serialized = "%s, %s;" % (tree.val, index)
+    if tree.left:
+        serialized += serialize(tree.left, index=index + ".1")
+    
+    if tree.right:
+        serialized += serialize(tree.right, index=index + ".2")
 
-node_serialized = serialize(node)
-print(node_serialized)
+    return serialized
+
+def deserialize(tree):
+    nodes = tree.split(";")
+    node_dict = {}
+    for node in nodes[-2::-1]:
+        [name, val] = node.split(",")
+        node_dict[val.strip()] = Node(name)
+
+    root_node = None
+
+    for key, value in node_dict.items():
+        if "." in key:
+            last_dot_index = key.rindex(".")
+            parent = key[:last_dot_index]
+            child = key[last_dot_index+1:]
+
+            parent_node = node_dict[parent]
+            if child == '1':
+                parent_node.left = value
+            else:
+                parent_node.right = value
+        
+        else:
+            return value
+
+
+
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+print("Success")
