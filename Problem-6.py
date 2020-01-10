d27c729f5d2220d8dbb14822c9a24953e8551259
Parth Pandya <parth.pandya@valensdatalabs.com>
2020-01-10 19:38:26 +0530

    Adds one problem and solution to Problem-6

--- Problem-6.py
+++ Problem-6.py
@@ -13,3 +13,24 @@ def cons(a, b):
 
 Implement car and cdr.
 '''
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def return_first(a, b):
+    return a
+
+def return_last(a, b):
+    return b
+
+def car(f):
+    return f(return_first)
+
+def cdr(f):
+    return f(return_last)
+
+print(car(cons(3, 4)))
+
+print(cdr(cons(3, 4)))
