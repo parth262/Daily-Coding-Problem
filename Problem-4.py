fe881a0d3e7e9a401e29dc2bc11a67d1ff8b902f
Parth Pandya <parthrpandya98@gmail.com>
2020-01-26 15:47:28 +0530

    Adds Solution to Problem-4

--- Problem-4.py
+++ Problem-4.py
@@ -8,3 +8,28 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 
 You can modify the input array in-place.
 '''
+
+
+def find_first_missing(arr):
+        if not arr:
+            return 1
+
+        arr.append(0)
+        n = len(arr)
+        for i in range(n):
+            if arr[i] <= 0 or arr[i] >= n:
+                arr[i] = 0
+
+        print(arr)
+
+        for i in range(n):
+            arr[arr[i] % n] += n
+
+        print(arr)
+
+        for i in range(1, n):
+            if arr[i] < n:
+                return i
+
+
+print(find_first_missing([3, 4, 3, 1]))
