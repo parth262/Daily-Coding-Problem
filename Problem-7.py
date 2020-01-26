3ea62d8a0ce9e164a484b5b9c3dbbb0cb0b02284
Parth Pandya <parthrpandya98@gmail.com>
2020-01-26 16:51:10 +0530

    Adds solution for Problem-7

--- Problem-7.py
+++ Problem-7.py
@@ -8,3 +8,19 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 
 You can assume that the messages are decodable. For example, '001' is not allowed.
 '''
+
+def count_n(message):
+    if len(message) == 0 or len(message) == 1:
+        return 1
+    
+    if len(message) == 2:
+        if message[0] == '1' or (message[0] == '2' and int(message[1]) <= 6):
+            return 1
+        else:
+            return 0
+
+    total = count_n(message[1:]) + count_n(message[2:]) + 1
+    return total
+    
+
+print(count_n('1132'))
