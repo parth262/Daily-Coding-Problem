db38226345ec7fb9ca457d93de8a1577651fcefe
Parth Pandya <parth.pandya@valensdatalabs.com>
2020-01-27 19:55:53 +0530

    Adds solution for Problem-9 and few more problems

--- Problem-9.py
+++ Problem-9.py
@@ -8,3 +8,17 @@ For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1,
 
 Follow-up: Can you do this in O(N) time and constant space?
 '''
+
+def find_max_sum(arr): 
+	incl = 0
+	excl = 0
+	
+	for i in arr: 
+		new_excl = max(incl,excl)
+		incl = excl + i
+		excl = new_excl
+
+	return max(excl, incl) 
+
+# Driver program to test above function
+print(find_max_sum([-2, 1, 3]))
