74c27f7d48224ec6b16407323fa8e7cb0005ec3a
Parth Pandya <parth.pandya@valensdatalabs.com>
2020-01-31 20:14:00 +0530

    Half solution to Problem-11

--- Problem-11.py
+++ Problem-11.py
@@ -8,3 +8,63 @@ For example, given the query string de and the set of strings [dog, deer, deal],
 
 Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 '''
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+def calculate_hash(ch):
+    i = alphabets.index(ch.lower()) + 1
+    return str(i**2 + i + 41)
+
+
+hash_dict = {}
+
+
+def hash_broken_word(broken_words):
+    prev_hash = None
+    for word in broken_words:
+        word_hash = ""
+        for ch in word:
+            word_hash += calculate_hash(ch)
+        if prev_hash:
+            if prev_hash not in hash_dict:
+                hash_dict[prev_hash] = []
+            if word_hash not in hash_dict[prev_hash]:
+                hash_dict[prev_hash].append(word_hash)
+
+        prev_hash = word_hash
+    return prev_hash
+
+def break_and_hash_word(word):
+    word_hash = ""
+    broken_words = []
+    for i in range(len(word)):
+        broken_words.append(word[0:i+1])
+
+    hash_dict[hash_broken_word(broken_words)] = word
+
+def save_words_in_ds(words):
+    for word in words:
+        word_hashes=break_and_hash_word(word)
+
+save_words_in_ds(["dog", "donkey"])
+
+def get_value(input_hash):
+    value = hash_dict[input_hash]
+    if not isinstance(value, list):
+        return value
+    return tuple(map(lambda v: get_value(v), value))
+
+def autocomplete(input):
+    input_hash = ""
+    for ch in input:
+        input_hash += calculate_hash(ch)
+
+    suggestions = get_value(input_hash)
+    print(suggestions)
+
+autocomplete("d")
+
+
+
+
+        
